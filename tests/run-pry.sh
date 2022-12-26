01dcba95980a4030d71c07a189d5cc4f550da0f8
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2022-12-26 14:13:59 +0100

    pdb

--- tests/run-pry.sh
+++ tests/run-pry.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-gdb --init-eval-command=pry | tee /dev/stderr | grep 'Executing pry in the inferior'
+set -e
+
+OUTPUT="$(gdb)"
+for plugin_name in python ruby; do
+    echo "$OUTPUT" | grep -o "$plugin_name" || exit 1
+done
+
+gdb --init-eval-command=pry | grep 'Executing pry in the inferior'
